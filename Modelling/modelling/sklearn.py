b6782a235749e4d9b162c8e604a70c3d35b8d386
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-15 13:44:32 +0530

    Update sklearn.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Modelling/modelling/sklearn.py
+++ Modelling/modelling/sklearn.py
@@ -4,24 +4,18 @@ from Modelling import *
 class Sklearn_Modelling:
     def train(self, model, X_train, y_train, X_test, y_test, name):
         wandb.init(project=PROJECT_NAME, name=name)
-        model = model()
         model.fit(X_train, y_train)
         preds = model.predict(X_test)
-        preds_prob = model.predict_prob(X_test)
-        print(list(y_test))
-        wandb.sklearn.plot_confusion_matrix(y_test, preds, y_test)
-        wandb.sklearn.plot_classifier(
-            model,
-            X_train,
-            X_test,
-            y_train,
-            y_test,
-            preds,
-            preds_prob,
-            labels,
-            model_name=f"{model}",
-            feature_names=None,
-        )
+        preds_prob = model.predict_proba(X_test)
+        labels = sorted(set(list(y_test)))
+        wandb.log({
+            "Accuracy":accuracy_score(preds,y_test),
+            "F1 Score":f1_score(preds,y_test),
+            "Precisions":precision_score(preds,y_test)
+        })
+        wandb.sklearn.plot_confusion_matrix(y_test, preds, labels)
+        wandb.sklearn.plot_classifier(model, X_train, X_test, y_train, y_test, preds, preds_prob, {0:0,1:1},
+                                                         model_name=f'{model}')
         wandb.sklearn.plot_roc(y_test, preds_prob, labels)
         wandb.sklearn.plot_precision_recall(y_test, preds_prob, labels)
         wandb.sklearn.plot_confusion_matrix(y_test, preds, labels)
