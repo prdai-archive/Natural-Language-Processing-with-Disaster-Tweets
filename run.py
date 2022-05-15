f581c49b9fbb9b98f171799df713929de98cc81e
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-15 11:08:02 +0530

    Update run.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from Modelling import *
+
 sdl = Sklearn_Data_Loader()
-sdl.create()
+X_train, X_test, y_train, y_test, vectorizer = sdl.create()
+sm = Sklearn_Modelling()
+sm.train(X_train, X_test, y_train, y_test,"BaseLine")
