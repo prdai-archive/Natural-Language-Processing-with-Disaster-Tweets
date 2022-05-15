9e7715f5d425f58fdda7a436c9b9400786023fa4
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-15 11:07:47 +0530

    Update pytorch.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Modelling/metrics/pytorch.py
@@ -0,0 +1,19 @@
+from Modelling import *
+
+
+def accuracy(model, X, y):
+    correct = 0
+    total = 0
+    preds = model(X.float())
+    for pred, y_batch in zip(preds, y):
+        pred = int(torch.round(pred))
+        y_batch = int(y_batch)
+        if pred == y_batch:
+            correct += 1
+        total += 1
+    acc = round(correct / total, 3) * 100
+    return acc
+def g_loss(model,X,y):
+    preds = model(X.float())
+    loss = criterion(preds.view(-1),y.float().view(-1))
+    return loss.item()
