ed887212e6d125ee529a6273a77e565b4af0c788
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-17 17:31:20 +0530

    Update run.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ X_train, X_test, y_train, y_test, vectorizer = sdl.create()
 
 model = svm.SVC(C=16, kernel="linear", gamma="auto",probability=True)
 sm = Sklearn_Modelling()
-sm.train(model,X_train, y_train, X_test, y_test, "BaseLine")
+model, preds, preds_prob= sm.train(model,X_train, y_train, X_test, y_test, "BaseLine")
+sdl.create_submission(model)
