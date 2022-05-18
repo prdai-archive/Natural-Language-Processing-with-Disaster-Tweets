aa11ffaf1d968c5cc09700f82b40dd82c89caa84
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-18 09:21:46 +0530

    Update sklearn.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Modelling/modelling/sklearn.py
+++ Modelling/modelling/sklearn.py
@@ -8,14 +8,16 @@ class Sklearn_Modelling:
         preds = model.predict(X_test)
         preds_prob = model.predict_proba(X_test)
         labels = sorted(set(list(y_test)))
-        wandb.log({
-            "Accuracy":accuracy_score(preds,y_test),
-            "F1 Score":f1_score(preds,y_test),
-            "Precisions":precision_score(preds,y_test)
-        })
+        wandb.log(
+            {
+                "Accuracy": accuracy_score(preds, y_test),
+                "F1 Score": f1_score(preds, y_test),
+                "Precisions": precision_score(preds, y_test),
+            }
+        )
         wandb.sklearn.plot_confusion_matrix(y_test, preds, labels)
-        wandb.sklearn.plot_classifier(model, X_train, X_test, y_train, y_test, preds, preds_prob, {0:0,1:1},
-                                                         model_name=f'{model}')
+        # wandb.sklearn.plot_classifier(model, X_train, X_test, y_train, y_test, preds, preds_prob, {0:0,1:1},
+        #                                                  model_name=f'{model}')
         wandb.sklearn.plot_roc(y_test, preds_prob, labels)
         wandb.sklearn.plot_precision_recall(y_test, preds_prob, labels)
         wandb.sklearn.plot_confusion_matrix(y_test, preds, labels)
