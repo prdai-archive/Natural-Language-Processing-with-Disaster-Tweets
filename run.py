bf0dc443f30536a1708f2885d7120e8c093cc9c2
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-18 13:10:41 +0530

    Update run.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from Modelling import *
 
-pdl = Pytorch_Data_Loader()
-new_X, new_y, X, y, X_train, X_test, y_train, y_test, all_words = pdl.create()
-pm = Pytorch_Modelling()
-model = pm.train(X_train, X_test, y_train, y_test, all_words=all_words)
-submission = pdl.create_submission(model)
-submission.to_csv("./Pytorch-BaseLine.csv", index=False)
+tdl = TensorFlow_Data_Loader()
+X_train, X_test, y_train, y_test, = tdl.create()
+tm = TensorFlow_Modelling()
+model = tm.train(X_train, X_test, y_train, y_test,)
+submission = tdl.create_submission(model)
+submission.to_csv("./TensorFlow-BaseLine.csv", index=False)
 
 # SKlearn = 77%
-# Pytorch =
+# Pytorch = 60%
+# Ten
