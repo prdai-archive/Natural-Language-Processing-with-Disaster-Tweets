d227558556bebc2ecdae85b28e706b02e922f1ed
Programmer-RD-AI <go2ranuga@gmail.com>
2022-05-20 06:53:09 +0530

    Update run.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,11 +1,25 @@
 from Modelling import *
 
-tdl = TensorFlow_Data_Loader()
-X_train, X_test, y_train, y_test, = tdl.create()
-tm = TensorFlow_Modelling()
-model = tm.train(X_train, X_test, y_train, y_test,)
-submission = tdl.create_submission(model)
-submission.to_csv("./TensorFlow-BaseLine.csv", index=False)
+models = [
+    "https://tfhub.dev/google/nnlm-en-dim50/2",
+    "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/2",
+    "https://tfhub.dev/digitalepidemiologylab/covid-twitter-bert/2",
+    "https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3",
+    "https://tfhub.dev/google/tf2-preview/nnlm-en-dim128/1",
+]
+models = ["https://tfhub.dev/digitalepidemiologylab/covid-twitter-bert/2"]
+for model in models:
+    tdl = TensorFlow_Data_Loader()
+    (
+        X_train,
+        X_test,
+        y_train,
+        y_test,
+    ) = tdl.create()
+    tm = TensorFlow_Modelling()
+    model = tm.train(X_train, X_test, y_train, y_test, model=model)
+    submission = tdl.create_submission(model)
+    submission.to_csv(f"./save/{model}-TensorFlow-BaseLine.csv", index=False)
 
 # SKlearn = 77%
 # Pytorch = 60%
